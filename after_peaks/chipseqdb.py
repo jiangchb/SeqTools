c4238c29f89eb59aebb45f3ce58d85d1438ee3f6
vhsvhs <victorhansonsmith@gmail.com>
2015-06-10 12:09:28 -0700

    .

--- after_peaks/chipseqdb.py
+++ after_peaks/chipseqdb.py
@@ -490,7 +490,7 @@ def import_foldenrichment(bdgpath, repid, con):
             curr_chromname = chromname
             curr_chromid = chromid
             last_start_site = 0
-            print "->", curr_chromname
+            print "\n\t", curr_chromname
                                          
             if curr_chromid not in chromid_summitsites:
                 chromid_summitsites[curr_chromid] = []
@@ -534,7 +534,7 @@ def import_foldenrichment(bdgpath, repid, con):
         """Can we map this enrichment site to a summit?"""        
         summit_here = False
         for ii in range(start, stop):
-            if curr_chromname == "chrI":
+            if curr_chromname == "pst_Chr_1":
                 if ii >= 182427 and ii <= 182427 + 100:
                     # PICST_66237|186773|182427|9|-
                     print l
