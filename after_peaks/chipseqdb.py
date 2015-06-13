32e1ad2807bfaa4eaa64206acd4a5390958dfc0a
vhsvhs <victorhansonsmith@gmail.com>
2015-06-13 12:07:55 -0700

    .

--- after_peaks/chipseqdb.py
+++ after_peaks/chipseqdb.py
@@ -773,7 +773,7 @@ def validate_summits_fe(repid, con):
         print "\n. Warning: I found no FE values for", x.__len__().__str__(), "genes."
         for ii in x:
             msg = "I found no FE values for gene " + ii[0].__str__() + ", replicate " + repid.__str__()
-            write_error(msg)
+            write_error(con, msg)
     
 
     #cur.execute("CREATE TABLE IF NOT EXISTS Genes(id INTEGER primary key autoincrement, name TEXT COLLATE NOCASE, start INT, stop INT, chrom INT, strand TEXT)")
