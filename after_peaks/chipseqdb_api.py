da5b86a23147ea326fea961746fc583907e38dfe
vhsvhs <victorhansonsmith@gmail.com>
2015-06-20 13:43:00 -0700

    .

--- after_peaks/chipseqdb_api.py
+++ after_peaks/chipseqdb_api.py
@@ -44,8 +44,8 @@ def get_chrom_id(con, name, speciesid):
 #         cur.execute(sql)
 #         x = cur.fetchone() 
 #         if x == None:
-        print sql
-        print "\n. 361 - Chromosome", name, "doesn't exist." 
+        #print sql
+        #print "\n. 361 - Chromosome", name, "doesn't exist." 
         return None
     else:
         return x[0]
