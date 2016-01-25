8822c6a933c1347580687f0a12f83e5de88d954c
vhsvhs <victorhansonsmith@gmail.com>
2016-01-24 22:27:05 -0800

    Adding motif code.

--- after_illumina/annotation_db.py
+++ after_illumina/annotation_db.py
@@ -441,7 +441,7 @@ def import_configuration(cpath, con):
             sql += "'" + speciesid.__str__() + "','" + gpath + "')"
             cur.execute(sql)
             con.commit()
-            print ". I found a genome for species", speciesid, "at", gpath
+            #print ". I found a genome for species", speciesid, "at", gpath
 
     """Parse lines for BOWTIEINDEX entries"""
     for ll in lines:
@@ -459,7 +459,7 @@ def import_configuration(cpath, con):
             sql += "'" + speciesid.__str__() + "','" + ipath + "')"
             cur.execute(sql)
             con.commit()
-            print ". I found a Bowtie index for species", speciesid, "at", ipath
+            #print ". I found a Bowtie index for species", speciesid, "at", ipath
     
     """Parse lines for GFF entries"""
     for ll in lines:
